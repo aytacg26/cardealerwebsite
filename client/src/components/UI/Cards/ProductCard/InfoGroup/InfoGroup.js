721664b84da7a58208526d78d33fe5569f528e9a
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-03-14 21:25:52 +0200

    Changed According to Mobile Control

--- client/src/components/UI/Cards/ProductCard/InfoGroup/InfoGroup.js
+++ client/src/components/UI/Cards/ProductCard/InfoGroup/InfoGroup.js
@@ -2,14 +2,27 @@ import React, { Fragment } from 'react';
 import classes from './infoGroup.module.css';
 import PropTypes from 'prop-types';
 
-const InfoGroup = ({ title, data }) => {
+const InfoGroup = ({ title, data, mobile }) => {
+  const labeling = !mobile ? (
+    <Fragment>
+      <label>
+        <strong>{title}</strong>
+      </label>
+      <span className='mg-l-5 mg-r-5'>
+        <strong>:</strong>
+      </span>
+    </Fragment>
+  ) : null;
+
+  const infoClass = !mobile
+    ? `${classes.InfoGroup}`
+    : `${classes.InfoGroupMobile}`;
+
   return (
     <Fragment>
       {data ? (
-        <div className={classes.InfoGroup}>
-          <label>
-            <strong>{title}</strong>
-          </label>
+        <div className={infoClass}>
+          {labeling}
           <span>{data}</span>
         </div>
       ) : null}
