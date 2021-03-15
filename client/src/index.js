ce444ae8a4ab7754cb764a671a797721aaab5b51
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-03-15 23:17:27 +0200

    Menu State added

--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import MenuState from './context/menuContext/MenuState';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MenuState>
+      <App />
+    </MenuState>
   </React.StrictMode>,
   document.getElementById('root')
 );
